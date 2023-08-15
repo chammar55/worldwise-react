e2a3a71155b78b3f631e82e6d6ed53da5f96abc2
chammar55 <chammar5255@gmail.com>
2023-08-15 12:28:22 +0500

    10

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,24 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        {/* index is default route */}
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="/login" element={<Login />} />
+        {/* Nested Routes */}
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         {/* It will show when one of upper pages are not selected  */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
