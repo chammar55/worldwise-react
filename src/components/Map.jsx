981d1fa236ab655ec3f77c06164be9ca55cae24a
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2023-08-18 23:38:16 +0500

    15

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,100 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+} from "react-leaflet"; // For Map
+import { useEffect, useState } from "react";
+import { useCities } from "../Contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
+  const {
+    isLoading: isLoadingPosition, // renaming them
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
+  const [mapLat, mapLng] = useUrlPosition(); // custom hook
+  // when the mapLat, mapLng value change we want to store it, so evenwe go back map will show the previous stored location
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
+  return (
+    // By using navigate hook we are moving to form page using URL
+    <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
+
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
+    </div>
+  );
+}
+
+// Custom hook
+// This function helps us to move map to the selected city
+function ChangeCenter({ position }) {
+  const map = useMap(); // this hook belong to leaflet library
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
   // this to move to specific URL
   // This is also a react-router hook
   const navigate = useNavigate();
-  // this hook getting data from URL
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-  return (
-    // By using navigate hook we are moving to form page using URL
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 20 })}>
-        Change pos
-      </button>
-    </div>
-  );
+
+  // This useMapEvent also belong to leaflet library
+  useMapEvent({
+    // wrap below code in {} and Console.log(e) then we can see all the data related Click on map, From here we are geting lat and lng values
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 
 export default Map;
